4f39c2126d8bd7abd1865d611fdd67e9bec6bcb8
akira <a.oono.sys23@morijyobi.ac.jp>
2025-07-14 21:02:10 +0900

    fix: resolve component rendering error

--- app/func-cache/page.tsx
+++ app/func-cache/page.tsx
@@ -1,16 +1,4 @@
-import { getServerTime } from '@/lib/getTime'
-
-// このコンポーネントはサーバーサイドでレンダリングされます
-async function ComponentA() {
-  const time = await getServerTime()
-  return <p>ComponentA サーバー時間: {time}</p>
-}
-
-// このコンポーネントもサーバーサイドでレンダリングされます
-async function ComponentB() {
-  const time = await getServerTime()
-  return <p>ComponentB サーバー時間: {time}</p>
-}
+import { ComponentA, ComponentB } from './ui/time-components'
 
 export default async function Page() {
   return (
