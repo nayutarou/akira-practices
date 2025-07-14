9d132fa2416960fafdbe872f622fa3f0e350d018
akira <a.oono.sys23@morijyobi.ac.jp>
2025-07-14 21:01:44 +0900

    refactor: use Link component instead of a tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import LayoutTopCache from './top-cache/layout-top-cache';
 import LayoutFuncCache from './func-cache/layout-func-cache';
 import { getServerTime } from "@/lib/getTime";
+import Link from 'next/link';
 
 export default function Page() {
   return (
@@ -14,6 +15,9 @@ export default function Page() {
         <p>関数内キャッシュ適用中の子要素</p>
         <p>サーバー時間 : {getServerTime()}</p>
       </LayoutFuncCache>
+
+      <Link href="/func-cache">Func Cache パージへ</Link>
+
     </>
   );
 }
